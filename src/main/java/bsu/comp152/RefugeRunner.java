f9efcc8ef04a9295dfe7a24f4bb97fa5d125a090
andyj <andy.janedy@gmail.com>
2023-03-12 18:38:18 -0400

    added switch statement.  Program will not build

--- src/main/java/bsu/comp152/RefugeRunner.java
+++ src/main/java/bsu/comp152/RefugeRunner.java
@@ -1,4 +1,5 @@
 package bsu.comp152;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 /**
@@ -23,5 +24,43 @@ public class RefugeRunner {
 
     public void runRefuge(WildlifeRefuge wlr) {
         // FIXME
+        int selection = 0;
+
+        while (selection != 7) {
+        }
+            System.out.println();
+            System.out.println("Welcome to Wildlife Refuge");
+            System.out.println("===========================");
+            System.out.println("Would you like to...");
+            System.out.println("1: Hire a wildlife rehabilitator.");
+            System.out.println("2: Admit an animal");
+            System.out.println("3: Look up a rehabilitator");
+            System.out.println("4: Print list of animals");
+            System.out.println("5: Print a list of rehabilitators");
+            System.out.println("6: Release animals");
+            System.out.println("7: Exit");
+            System.out.println("Please enter a number 1-7");
+            selection = keyboard.nextInt();
+            keyboard.nextLine();
+
+            switch (selection) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    System.out.println("See you next time!");
+                    break;
+
+            }
+
     }
 }
